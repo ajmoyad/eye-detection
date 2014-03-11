cd1047f219ab09bd55c2b7b1216e700c0586091d
Antonio Moya <ajmoyad@gmail.com>
2014-03-11 11:41:45 +0100

    update tree

--- demo.m
+++ demo.m
@@ -6,30 +6,30 @@ warning off
 
 %% Resultado correcto
 % A continuación una serie de imágenes que presentan un resultado correcto.
-I1=imread('imagenes/yo2.png');
-I2=imread('imagenes/yo3.jpg');
-I3=imread('imagenes/yo.png'); 
-I4=imread('imagenes/cara3.jpg');
-I5=imread('imagenes/cara4.jpg');
+I1=imread('test/yo2.png');
+I2=imread('test/yo3.jpg');
+I3=imread('test/yo.png'); 
+I4=imread('test/cara3.jpg');
+I5=imread('test/cara4.jpg');
 
 %% Resultado erroneo
 % A continuación una serie de imágenes que presentan un resultado, pero
 % éste es erróneo
 
-I12=imread('imagenes/negro.jpg');
-I7=imread('imagenes/negra.jpg');
-I8=imread('imagenes/cara.jpg');
-I9=imread('imagenes/cara2.jpg');
-I10=imread('imagenes/ojo6.jpg');
-I11=imread('imagenes/lena.png');
-I6=imread('imagenes/nene.jpg');
+I12=imread('test/negro.jpg');
+I7=imread('test/negra.jpg');
+I8=imread('test/cara.jpg');
+I9=imread('test/cara2.jpg');
+I10=imread('test/ojo6.jpg');
+I11=imread('test/lena.png');
+I6=imread('test/nene.jpg');
 
 
 %% Incapaz de hallar resultado
 % Comentadas por que éstas imágenes generan una excepción.
-%I=imread('imagenes/Charlize.jpg'); % Es es la jodida % SOPRENDENTE CON APERTURA Y 0.3
-%I=imread('imagenes/carnet.jpg'); % Aceptable SIN apertura
-%I=imread('imagenes/Edu.jpg'); % GAFAS / IMPOSIBLE
+%I=imread('test/Charlize.jpg'); % Es es la jodida % SOPRENDENTE CON APERTURA Y 0.3
+%I=imread('test/carnet.jpg'); % Aceptable SIN apertura
+%I=imread('test/Edu.jpg'); % GAFAS / IMPOSIBLE
 
 
 img={'I1';'I2';'I3';'I4';'I5';'I6';'I7';'I8';'I9';'I10';'I11';'I12'};
